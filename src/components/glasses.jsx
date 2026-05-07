00658fc43c8119be227de579ae71b44c8137a355
Barmoo <armoobelinda92@gmail.com>
2026-05-07 22:34:18 +0000

    changes added

--- src/components/glasses.jsx
+++ src/components/glasses.jsx
@@ -29,7 +29,7 @@ const GlassesCard = ({ src, alt }) => (
         className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
       />
     </div>
-    <div className="absolute inset-0 bg-gradient-to-t from-black/10 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+    <div className="absolute inset-0 bg-linear-to-t from-black/10 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
   </div>
 );
 
